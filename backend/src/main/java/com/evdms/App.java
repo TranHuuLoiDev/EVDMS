73d6ddd5ac3db98717e0dc92ca3e21b796b64571
Dev Tran <tranhuuloi200510@gmail.com>
2025-10-29 00:36:42 +0700

    main folder

---
+++ backend/src/main/java/com/evdms/App.java
@@ -0,0 +1,13 @@
+package com.evdms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
